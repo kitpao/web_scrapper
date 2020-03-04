68b2f44ebee01ce90db7789f14346d150851df76
kitpao <kitpao91@gmail.com>
2020-03-04 11:22:22 -0600

    get data from 1 day

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,23 @@
 #!/usr/bin/env ruby
+
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
 
+def scraper
+  #get data from various days (4)
+    #get specific data, 3 elements
+  url = 'https://www.eldolar.info/es-MX/mexico/dia/hoy'
+  unparsed = HTTParty.get(url)
+  parsed = Nokogiri::HTML(unparsed)
+  today = {}
+  best_to_sell = parsed.css('[title="Mejor compra"]').text.to_f
+  best_to_buy = parsed.css('[title="Mejor venta"]').text.to_f
+  average = parsed.css('td.num.xTimes').text.to_f
+  today[:sell] = best_to_sell
+  today[:buy] = best_to_buy
+  today[:av] = average
+  byebug
+end
 
-puts "Hello world!"
+scraper
