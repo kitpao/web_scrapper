9f4bc69575fd4dc3668e7c24f7ba332cb5f5e29a
kitpao <kitpao91@gmail.com>
2020-03-05 02:53:55 -0600

    clean main.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -10,83 +10,17 @@ require 'timeout'
 require_relative '../lib/extra_methods'
 require_relative '../lib/program_methods'
 
-def scraper(day_limit)
 
-  stats = []
-  day_counter = 0
-  day_limit = day_limit
+program = Program.new()
 
-  while day_counter < day_limit
-    #get day for URLs
-    date_string = Date.today-day_counter
-    #convert
-    formated_date = date_string.to_s.match /(\d+)-(\d+)-(\d+)/
-    ready_date = formated_date[1]+formated_date[2]+formated_date[3]
-
-    day_counter += 1
-
-    #specify route
-    url = 'https://www.eldolar.info/es-MX/mexico/dia/' + ready_date
-    unparsed = HTTParty.get(url)
-    parsed = Nokogiri::HTML(unparsed.body)
-
-    #retrieve data
-    this_day = []
-    best_to_sell = parsed.css('[title="Mejor compra"]').text.to_f
-    best_to_buy = parsed.css('[title="Mejor venta"]').text.to_f
-    average = parsed.css('td.num.xTimes').text.to_f
-    this_day[0] = best_to_sell
-    this_day[1] = best_to_buy
-    this_day[2] = average
-    stats << this_day
-  end
-
-  notify = 0
-  #comparison to sell
-  max_sell = stats[0][0]
-  (day_limit-1).times do |i|
-    max_sell = stats[i][0] if max_sell < stats[i][0]
-  end
-
-  if max_sell == stats[0][0] && max_sell > stats [0][2]
-    puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    puts "The price right now is the HIGHEST in the last #{day_limit} days! you should SELL right now at $#{max_sell} pesos per dollar, check the best place to sell in the fist column of this webpage: https://www.eldolar.info/es-MX/mexico/dia/hoy"
-    notify = 1
-    puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-  end
-
-  #comparison to buy
-  min_buy = stats[0][1]
-  (day_limit-1).times do |i|
-    min_buy = stats[i][1] if min_buy > stats[i][1]
-  end
-
-  if min_buy == stats[0][1] && min_buy < stats [0][2]
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    puts "The price right now is the LOWEST in the last #{day_limit} days! you should BUY right now at $#{min_buy} pesos per dollar, check the best place to buy in the second column of this webpage: https://www.eldolar.info/es-MX/mexico/dia/hoy"
-    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    notify = 1
-  end
-
-  if notify == 0
-    time = Time.new
-    puts "----There are no news now at " + time.inspect + " comparing against the data of the last #{day_limit} days----"
-    puts "Current dollar price for selling: #{stats [0][0]}, highest is #{max_sell}"
-    puts "Current dollar price for buying: #{stats [0][1]}, lowest is #{min_buy}"
-    puts "Average price: #{stats [0][2]}"
-  end
-  #result
-  stats
-
-end
 #welcome and setup
 puts "Welcome! This program will inform you when the exchange rate between Mexican pesos and Dollars are at the best! it will check every hour"
+puts "_______________________________________________________________"
 sleep (1)
 puts ""
 check_urgency = true
 while check_urgency
   puts "Please type a number and press enter to see how fast would you like to exchange your money:"
-  sleep (1.4)
   puts "1 - Very fast - Quicker notifications but more volatile since it only takes the rates from the last 4 days"
   puts "2 - Fast - Quick notifications and less volatile since it is based on the last 7 days"
   puts "3 - Normal - Slow notifications but stable since it is based on the last 2 weeks"
@@ -94,7 +28,7 @@ while check_urgency
   puts ""
   print "Your answer: "
   begin
-    input = gets.chomp.match("[1-4]").string
+    input = gets.chomp.match("^[1-4]$").string
   rescue
     puts "REMEMBER TO type a number from 1 to 4"
     puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
@@ -110,7 +44,7 @@ sleep(1)
 puts "Processing..."
 
 while true
-  scraper(day_limit) #program.scrape
+  program.run_program(day_limit)
   puts "If you would like to change how fast you get positive notifications, please type the new number in the next 20 seconds (1 - Very fast, 2 - Fast, 3 - Normal, 4 - Long run), if not, it will continue working as normal"
 
   begin
@@ -121,7 +55,7 @@ while true
     puts "No new answer..."
   else
     day_limit = choose_days(user_answer, day_limit)
-    if user_answer.to_i >= 1 && user_answer <= 4
+    if user_answer.to_i >= 1 && user_answer.to_i <= 4
       puts "New parameter received: " + user_answer
     else
       puts "Invalid answer, using previous value #{input}"
