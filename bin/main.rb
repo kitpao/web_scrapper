7a76a6129125d3e9974877dc0ecf9d3df49af011
kitpao <kitpao91@gmail.com>
2020-07-19 05:40:59 +0000

    working

--- bin/main.rb
+++ bin/main.rb
@@ -10,75 +10,37 @@ require_relative '../lib/program_methods'
 program = Program.new()
 
 # welcome and setup
-puts "Welcome! This program will inform you when the exchange rate between Mexican pesos and Dollars are at the best! it will check every hour"
+puts "This is a modification to check for updates"
 puts "_______________________________________________________________"
-sleep (1)
-puts ""
-check_urgency = true
-while check_urgency
-  puts "Please type a number and press enter to see how fast would you like to exchange your money:"
-  puts "1 - Very fast - Quicker notifications but more volatile since it only takes the rates from the last 4 days"
-  puts "2 - Fast - Quick notifications and less volatile since it is based on the last 7 days"
-  puts "3 - Normal - Slow notifications but stable since it is based on the last 2 weeks"
-  puts "4 - Long run - Slower notifications but the most reliable, since it takes the data from the last 30 days"
-  puts ""
-  print "Your answer: "
-  begin
-    input = gets.chomp.match("^[1-4]$").string
-  rescue
-    puts "REMEMBER TO type a number from 1 to 4"
-    puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-    sleep(1.2)
-  else
-    day_limit = choose_days(input, 0)
-    check_urgency = false
-  end
-end
 
 puts "NOTE: to end the program just press ctrl + c and close the window"
 sleep(1)
 puts "Processing..."
-
+page = program.start
 while true
-  program.run_program(day_limit)
-  puts "If you would like to change how fast you get positive notifications, please type the new number in the next 20 seconds (1 - Very fast, 2 - Fast, 3 - Normal, 4 - Long run), if not, it will continue working as normal"
-
-  begin
-    user_answer = Timeout::timeout(20) do
-      gets.chomp
-    end
-  rescue Timeout::Error
-    puts "No new answer..."
-  else
-    day_limit = choose_days(user_answer, day_limit)
-    if user_answer.to_i >= 1 && user_answer.to_i <= 4
-      puts "New parameter received: " + user_answer
-      input = user_answer
-    else
-      puts "Invalid answer, using previous value #{input}"
-    end
-  end
-  puts "waiting one hour for new results"
+  result = program.update(page)
+  puts result
+  puts "waiting one minute for new results"
   print "[0%_"
-  sleep(358)
+  sleep(6)
   print "_10%_"
-  sleep(358)
+  sleep(6)
   print "_20%_"
-  sleep(358)
+  sleep(6)
   print "_30%_"
-  sleep(358)
+  sleep(6)
   print "_40%_"
-  sleep(358)
+  sleep(6)
   print "_50%_"
-  sleep(358)
+  sleep(6)
   print "_60%_"
-  sleep(358)
+  sleep(6)
   print "_70%_"
-  sleep(358)
+  sleep(6)
   print "_80%_"
-  sleep(358)
+  sleep(6)
   print "_90%_"
-  sleep(358)
+  sleep(6)
   print "_100%]"
   puts ""
 end
--- lib/program_methods.rb
+++ lib/program_methods.rb
@@ -4,96 +4,88 @@
 require 'date'
 require 'nokogiri'
 require 'httparty'
+require 'mechanize'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+require 'byebug'
+#brew install mpg123
+#mpg123 alarm.wav
+
+Capybara.default_driver = :poltergeist
+Capybara.run_server = false
 
 class Program
+  include Capybara::DSL
+
   attr_reader :day_limit
   def initialize
     @day_limit = day_limit
   end
 
-  def run_program(day_limit)
-    stats = scraper(day_limit)
-    notify_results(stats, day_limit)
+  def start()
+    page = log_in_with_me()
+  end
+
+  def update(login)
+    update = update_page(login)
   end
 
   private
 
-  def scraper(day_limit, day_counter = 0)
-    stats = []
-    while day_counter < day_limit
-      # get day for URLs
-      date_string = Date.today - day_counter
-      # format day for url
-      formated_date = date_string.to_s.match(/(\d+)-(\d+)-(\d+)/)
-      ready_date = formated_date[1] + formated_date[2] + formated_date[3]
-      day_counter += 1
-      # specify route
-      url = 'https://www.eldolar.info/es-MX/mexico/dia/' + ready_date
-      unparsed = HTTParty.get(url)
-      parsed = Nokogiri::HTML(unparsed.body)
-      # retrieve data
-      this_day = []
-      best_to_sell = parsed.css('[title="Mejor compra"]').text.to_f
-      best_to_buy = parsed.css('[title="Mejor venta"]').text.to_f
-      average = parsed.css('td.num.xTimes').text.to_f
-      return scraper(day_limit + 1, 1) if best_to_sell == 0.0 && best_to_buy == 0.0
-
-      this_day[0] = best_to_sell
-      this_day[1] = best_to_buy
-      this_day[2] = average
-      stats << this_day
-    end
-    stats
+  def log_in_with_me
+    # log in
+    url = 'https://dashboard.microverse.org/auth/auth0'
+    visit(url)
+    puts "I am filling the form"
+    fill_in '1-email', with: File.read('user.rb')
+    sleep(2)
+    puts "done submitting username"
+    fill_in 'password', with: File.read('password.rb')
+    sleep(2)
+    puts "done submitting password"
+    click_link_or_button 'submit'
+    sleep(5)
+    puts "done submitting form"
+    return page
   end
 
-  def notify_results(stats, day_limit)
-    notify = 0
-    max_sell = search_highest(stats, day_limit)
-    min_buy = search_lowest(stats, day_limit)
-
-    if max_sell == stats[0][0] && max_sell > stats[0][2]
-      puts '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-      puts "The price right now is the HIGHEST in the last #{day_limit} days!"
-      puts "you should SELL right now at $#{max_sell} pesos per dollar,"
-      puts 'check the best place to sell in the fist column of this webpage: https://www.eldolar.info/es-MX/mexico/dia/hoy'
-      notify = 1
-      puts '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+  def update_page(page)
+    begin
+      page.click_on 'Code Review Requests'
+    rescue
+      sleep(2)
+      page.current_path
+    else
+      puts "no error handling"
+    ensure
+      page.current_path
     end
-
-    if min_buy == stats[0][1] && min_buy < stats[0][2]
-      puts '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-      puts "The price right now is the LOWEST in the last #{day_limit} days!"
-      puts "you should BUY right now at $#{min_buy} pesos per dollar,"
-      puts 'check the best place to buy in the second column of this webpage: https://www.eldolar.info/es-MX/mexico/dia/hoy'
-      puts '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
-      notify = 1
+    puts "done with first error handling"
+    begin
+      page.click_on 'Code Review Requests'
+    rescue
+      sleep(2)
+      page.current_path
+    else
+      puts "no error handling"
+    ensure
+      page.current_path
     end
-
-    if notify.zero?
-      time = Time.new
-      puts '-----There are no news now at ' + time.inspect + " comparing against the data of the last #{day_limit} days"
-      puts "Current dollar price for selling: #{stats[0][0]}, highest is #{max_sell}"
-      puts "Current dollar price for buying: #{stats[0][1]}, lowest is #{min_buy}"
-      puts "Average price: #{stats[0][2]}"
+    puts "you are in this link:"
+    puts page.current_path
+    puts "successfull link to code review"
+    if page.has_css? '.review-request-table-link'
+      puts "YOU FOUND THE LINK!!!!!!!!!!!!"
     end
-    notify
+  
+    if page.has_css? '.review-request-button'
+      puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FOUND!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+    else
+      puts "-----------------------------NOTHING FOR NOW---------------------------------------"
+    end
+    
+    return page.has_css? '.review-request-button'
   end
 
-  def search_highest(stats, day_limit)
-    # comparison to sell
-    max_sell = stats[0][0]
-    (day_limit - 1).times do |i|
-      max_sell = stats[i][0] if max_sell < stats[i][0]
-    end
-    max_sell
-  end
-
-  def search_lowest(stats, day_limit)
-    # comparison to buy
-    min_buy = stats[0][1]
-    (day_limit - 1).times do |i|
-      min_buy = stats[i][1] if min_buy > stats[i][1]
-    end
-    min_buy
-  end
 end
